2038a5e6b5a9d95352e8add997bfb7d7a7e5f2a6
Peter Mai <pm123633@gmail.com>
2021-11-19 11:48:35 -0600

    Switch to SQLite3

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,7 @@
-const config = require("./index");
-
-const db = config.db;
-const username = db.username;
-const password = db.password;
-const database = db.database;
-const host = db.host;
-
 module.exports = {
   development: {
-    username,
-    password,
-    database,
-    host,
-    dialect: "postgres",
+    storage: 'db/database.db',
+    dialect: 'sqlite',
+    seederStorage: 'sequelize',
   },
 };
